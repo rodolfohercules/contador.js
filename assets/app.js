6f844ac2b913975ece902ef931c849225d9455b9
Rodolfo Hercule <rodolfohercules@gmail.com>
2023-02-27 19:15:46 -0300

    Commit contador

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+let count = 0
+
+const CURRENT_NUMBER = document.getElementById('currentNumber')
+
+function increment() {
+  count++
+  CURRENT_NUMBER.innerHTML = count
+}
+
+function decrement() {
+  count--
+  CURRENT_NUMBER.innerHTML = count
+}
+
+function test() {}
+
